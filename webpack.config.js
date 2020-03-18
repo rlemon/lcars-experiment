e246261ded95e4a7d99177f1b8eef827798fa798
rlemon <rob.lemon@gmail.com>
2020-03-17 20:27:00 -0400

    new project

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const { WebpackBundleSizeAnalyzerPlugin } = require('webpack-bundle-size-analyzer');
+
+const MODE = process.env.NODE_ENV || 'development';
+
+const config = {
+	mode: MODE,
+
+	entry: {
+		app: [
+			'./src'
+		]
+	},
+
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'assets/entry.[name].[hash].js',
+		chunkFilename: 'assets/dependancy.[id].[chunkhash].js',
+		publicPath: '/'
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.s?css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+				sideEffects: true
+			},
+			{
+				test: /.(png|jpe?g|gif|svg|stl)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'assets/[hash].[ext]'
+						}
+					}
+				]
+			}
+		]
+	},
+
+	plugins: [
+		new WebpackBundleSizeAnalyzerPlugin('./webpack-report.txt'),
+		new HtmlWebpackPlugin({
+			chunks: ['app'],
+			filename: 'index.html',
+			template: 'src/template.html',
+			title: 'lcars'
+		})
+	],
+
+	devtool: 'source-map',
+
+	devServer: {
+		contentBase: './dist',
+		watchContentBase: true
+	}
+
+};
+
+if (MODE === 'production') {
+	config.plugins.push(new CompressionPlugin({
+		filename: '[path].gz[query]',
+		algorithm: 'gzip',
+		test: /\.js$|\.html$/,
+		threshold: 10240,
+		minRatio: 0.8
+	}));
+}
+
+module.exports = config;
